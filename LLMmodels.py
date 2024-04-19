f5b365d1605e878903c08de8bd8875bd36f03e01
Aunne <unihae236@gmail.com>
2024-04-19 17:49:14 +0800

    update to gemini 1.5

--- LLMmodels.py
+++ LLMmodels.py
@@ -1,40 +1,8 @@
 import google.generativeai as genai
 import json
 
-
-# if want to use local mistral LLM model,please remove those annotation
-# import random
-# import openai
-# openai.api_base = "http://127.0.0.1:4891/v1"
-# openai.api_key = "not needed for a local LLM"
-
-
 with open('setting.json', 'r', encoding='utf8') as jfile:
     Gemini_API_KEY = json.load(jfile)['Gemini_API_KEY']
-
-
-"""
-def mistral_completion(prompt):
-    # choose model
-    model = "mistral-7b-openorca.q4_0"
-# make amount of text
-    text_amount = 1
-# Make the API request
-    response = openai.Completion.create(
-        model=model,
-        prompt=prompt,
-        presence_penalty=1.7,
-        frequency_penalty=1.4,
-        max_tokens=200,
-        temperature=0.2,
-        top_p=0.6,
-        n=text_amount,
-        echo=True,
-        stream=False
-    ).choices[random.randrange(text_amount)].text.split("\n", 1)[1]
-
-    return response
-"""
 
 
 def gemini_chat(history, prompt):
@@ -42,7 +10,7 @@ def gemini_chat(history, prompt):
 
     # Set up the model
     generation_config = {
-        "temperature": 1,
+        "temperature": 0.9,
         "top_p": 0.4,
         "top_k": 10,
         "max_output_tokens": 2048,
@@ -54,9 +22,9 @@ def gemini_chat(history, prompt):
         "filter_prohibited_content": False
     }
 
-    model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+    model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
                                   generation_config=generation_config)
-
+    
     convo = model.start_chat(history=history)
     convo.send_message(prompt)
 
